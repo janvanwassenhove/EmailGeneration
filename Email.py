4d3976e5113c9a59b63c3132c5447a841e87ef5b
janvanwassenhove (mITy.John) <vanwassenhove.jan@gmail.com>
2023-02-25 10:17:17 +0100

    Document + Code email generator

---
+++ Email.py
@@ -0,0 +1,34 @@
+import random
+import string
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--scrambled', type=bool, default=False)
+args = parser.parse_args()
+
+def generate_email():
+    # List available domain names
+    domains = ["outlook.com", "yahoo.com", "cloudbreakers.com", "msn.com", "aol.com", "icloud.com", "gmx.com", "zoho.com", "gmail.com"]
+    # Choose random domain from list
+    domain = random.choice(domains)
+
+    if args.scrambled: # scrambled characters usersname
+        # Listing possible characteres username
+        letters = string.ascii_lowercase
+        # Generating random username of 10 characters length
+        username = ''.join(random.choice(letters) for i in range(10))
+    else : # more humanoid username
+        # List of adjectives and nouns to choose from
+        adjectives = ['vt', 'turn', 'funny', 'serious', 'smart', 'holy', 'cute', 'XY', 'lol', 'fyi']
+        nouns = ['frank', 'zita', 'turan', 'morgan', 'lion', 'tiger', 'local','cedric','nelly', 'tom', 'tim', 'tessa', 'bert', 'sam']
+        num1 = random.randint(1, 10)
+        num2 = random.randint(1, 10)
+
+        # Generate a random username by combining two words
+        username = random.choice(adjectives) + random.choice(nouns) + str(num1) + str(num2)
+
+    # Combinevan username & domain to get a complete e-mailadres
+    email = username + "@" + domain
+    return email
+
+print(generate_email())
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# EmailGeneration
-Generates custom email addresses (for testing purposes)
+### Simple Email generation
+If you only want to generate different email addresses, you can just directly use Email.py.</br>
+To run & generate a new email address:
+
+`python Email.py`
+
+There are two options to generate a mail address:</br>
+OR using scrambled characters, </br>
+OR using more 'human' names for generating the username.
+
+By default, more human names are chosen constructing the username as part of the mail address.
+You can play with 'adjectives' & 'nouns' list in the code to generate even more different email addresses.
+
+If you'd need scrambled characters for the user name, the 'scrambled' variable can be used:
+
+`python Email.py --scrambled=True`
